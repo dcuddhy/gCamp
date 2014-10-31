3b9682120948eccca46375a68e0802d9ffb0becf
dcuddhy <dcuddhy@gmail.com>
2014-10-30 22:16:38 -0600

    can add projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,39 @@
 class ProjectsController <ApplicationController
 
   def index
-    @project = Project.all
+    @projects = Project.all
   end
 
   def new
     @project = Project.new
   end
 
+  def create
+    project_params = params.require(:project).permit(:name)
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path, notice: 'User was successfully created.'
+  end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    project_params = params.require(:project).permit(:name)
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to projects_path
+  end
+
+def destroy
+  @project = Project.find(params[:id])
+  @project.destroy
+  redirect_to projects_path
+end
 
 end
