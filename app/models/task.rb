2b79d038f83069a931da6fe48aa90465caf73e91
dcuddhy <dcuddhy@gmail.com>
2014-11-11 14:49:46 -0700

    added form to user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ActiveRecord::Base
   validates :description, presence: true
 
-  validate :not_past_date
+  validate :not_past_date, on: :create
 
   def not_past_date
-    if self.due_date < Date.today
+    if due_date && due_date < Date.today
       errors.add(:due_date, 'cannot be in past')
     end
   end
