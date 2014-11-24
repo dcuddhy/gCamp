b13ccb4ce71ec7acc39823eb1adb685fb52c6809
dcuddhy <dcuddhy@gmail.com>
2014-11-24 15:37:09 -0700

    tighted up tabs on user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,48 +4,48 @@ require "rails_helper.rb"
 
   it "validates presence of user" do
     user = User.create(
-    first_name:"Jimmy",
-    last_name:"Jones",
-    email:"j@j.com",
-    password:"pass",
-    password_confirmation:"pass")
+      first_name:"Jimmy",
+      last_name:"Jones",
+      email:"j@j.com",
+      password:"pass",
+      password_confirmation:"pass")
     user.valid?
     expect(user.errors.present?).to eq(false)
   end
 
   it "validates uniqueness of user's email" do
     user = User.create(
-    first_name:"Jimmy",
-    last_name:"Jones",
-    email:"j@j.com",
-    password:"pass",
-    password_confirmation:"pass")
+      first_name:"Jimmy",
+      last_name:"Jones",
+      email:"j@j.com",
+      password:"pass",
+      password_confirmation:"pass")
     user.valid?
 
     user = User.create(
-    first_name:"Jermane",
-    last_name:"Johnson",
-    email:"j@j.com",
-    password:"pass",
-    password_confirmation:"pass")
+      first_name:"Jermane",
+      last_name:"Johnson",
+      email:"j@j.com",
+      password:"pass",
+      password_confirmation:"pass")
     expect(user.errors.present?).to eq(true)
   end
 
   it "validates uniqueness of user's email, despite case" do
     user = User.create(
-    first_name:"Jimmy",
-    last_name:"Jones",
-    email:"j@j.com",
-    password:"pass",
-    password_confirmation:"pass")
+      first_name:"Jimmy",
+      last_name:"Jones",
+      email:"j@j.com",
+      password:"pass",
+      password_confirmation:"pass")
     user.valid?
 
     user = User.create(
-    first_name:"Jermane",
-    last_name:"Johnson",
-    email:"J@j.com",
-    password:"pass",
-    password_confirmation:"pass")
+      first_name:"Jermane",
+      last_name:"Johnson",
+      email:"J@j.com",
+      password:"pass",
+      password_confirmation:"pass")
     expect(user.errors.present?).to eq(true)
   end
 
