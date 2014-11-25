61060b5897e22b8b784946a4a2d0f127f2d6485d
dcuddhy <dcuddhy@gmail.com>
2014-11-25 15:45:28 -0700

    added full name def for future ease

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
 
-validates :first_name, presence: true
-validates :last_name, presence: true
-validates :email, presence: true, uniqueness: true
-validates_uniqueness_of :email, case_sensitive: false
-  has_secure_password
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_uniqueness_of :email, case_sensitive: false
+    has_secure_password
 
   has_many :memberships
   has_many :projects, through: :memberships
