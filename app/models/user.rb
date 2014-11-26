a841145ca6c428b39568e96195f5180b0e22b7e7
dcuddhy <dcuddhy@gmail.com>
2014-11-25 21:02:57 -0700

    added error count to message

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   validates_uniqueness_of :email, case_sensitive: false
     has_secure_password
 
