c343edeaeb80bbcf5d23c668d2d05a06c67dc286
dcuddhy <dcuddhy@gmail.com>
2014-11-25 21:22:33 -0700

    added notice to application.html for added DRYness

--- app/views/tasks/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-
-validates :first_name, presence: true
-validates :last_name, presence: true
-validates :email, presence: true, uniqueness: true
-  has_secure_password
-
-end
