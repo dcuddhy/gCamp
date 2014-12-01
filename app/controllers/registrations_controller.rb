e7cf60ec428d38f7dfc87a39829a4ae0db53b74b
dcuddhy <dcuddhy@gmail.com>
2014-12-01 16:30:06 -0700

    after users sign up redirect to new project page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,7 +13,7 @@ class RegistrationsController < ApplicationController
       :password_confirmation))
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to new_project_path
     else
       render :new
     end
