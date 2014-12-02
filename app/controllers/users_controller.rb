58f09fb417b95727ffd52d93d2e536d2e7198dc3
dcuddhy <dcuddhy@gmail.com>
2014-12-02 15:25:15 -0700

    changed deleted user notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to users_path, notice: "User Deleted Successfully"
+    redirect_to users_path, notice: "User was successfully deleted"
   end
 
   layout :determine_layout
