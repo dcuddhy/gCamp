a66f76dc6bed21eb86e1d1756e2065777bbb9388
dcuddhy <dcuddhy@gmail.com>
2014-12-10 11:13:28 -0700

    membership delete test is finally tight

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -38,20 +38,16 @@ describe MembershipsController do
 
 # ----------------------> #DESTROY <----------------------------
 
-describe "#destroy" do
-  it "cannot destroy last owner" do
-    session[:id] = @owner.id
-    owner_count = @project.memberships.where('role=?', 'owner').count
-    delete :destroy, project_id: @project, id: @membership
-    expect(@project.memberships.where('role=?', 'owner').count).to eq(owner_count)
+  describe "#destroy" do
+    it "cannot destroy last owner" do
+      membership1 = @membership
+      session[:user_id] = membership1.user.id
+      owner_count = @project.memberships.where('role=?', 'owner').count
+      delete :destroy, project_id: membership1.project.id, id: membership1.id
+      expect(@project.memberships.where('role=?', 'owner').count).to eq(owner_count)
+    end
+
   end
-
-  ### This test passes for the wrong reason.  It isn't able to delete last
-  ### owner because 'are_you_logged_in' sends you to sign_in page instead
-  ### of executing destroy.  The code I wrote to make last user unable to
-  ### delete does in fact work... but the test gets derailed for some reason
-
-end
 
 
 
