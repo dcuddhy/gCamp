03ee33cf0ae110603de1870d366041dd81d254b4
dcuddhy <dcuddhy@gmail.com>
2014-12-17 12:58:00 -0700

    added helper to use blocks for consistency in headers universally

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def page_header(title, &block)
+    content_for(:title, title)
+    html = '<div class = "page-header">'.html_safe
+
+    html += '<div class = "pull-right">'.html_safe
+    html += capture do
+      block.call
+    end
+    html += '</div>'.html_safe
+
+    html += '<h2>'.html_safe
+    html += title
+    html += '</h2>'.html_safe
+    html += '</div>'.html_safe
+    html
+  end
+
+
 end
