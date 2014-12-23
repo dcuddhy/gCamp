43a7fc7084ae50f73a6ac094eaf59fead18259a2
dcuddhy <dcuddhy@gmail.com>
2014-12-23 14:26:08 -0700

    Users with tracker tokens can see stories for tracker projects

--- app/controllers/tracker_projects_controller.rb
+++ app/controllers/tracker_projects_controller.rb
@@ -4,4 +4,12 @@ class TrackerProjectsController < ApplicationController
     tracker_api = TrackerAPI.new
   end
 
+  def show
+    tracker_api = TrackerAPI.new
+    @tracker_projects = tracker_api.projects(current_user.pivotal_tracker_token)
+    @tracker_stories = tracker_api.stories(current_user.pivotal_tracker_token, params[:id])
+  end
+
+
+
 end
--- app/models/tracker_api.rb
+++ app/models/tracker_api.rb
@@ -25,9 +25,6 @@ class TrackerAPI
   end
 
 
-
-
-
 end
 
 
